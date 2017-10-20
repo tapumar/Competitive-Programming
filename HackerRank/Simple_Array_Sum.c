b8c6555cdb484763cf57d0404bd6cfb3b61b0f3f
Talitha Pumar <tapumar@gmail.com>
2017-10-20 14:56:02 -0200

    Create Simple_Array_Sum.c

---
+++ HackerRank/Simple_Array_Sum.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n,soma=0; 
+    scanf("%d",&n);
+    int arr[n];
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       scanf("%d",&arr[arr_i]);
+        soma +=arr[arr_i];
+    }
+    printf("%d\n",soma);
+    return 0;
+}
