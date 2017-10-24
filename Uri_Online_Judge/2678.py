f27ca9bf634d97194e210fc90e32e7f3794d9ed2
Talitha Pumar <tapumar@gmail.com>
2017-10-24 13:57:55 -0200

    Create 2678.py

---
+++ Uri_Online_Judge/2678.py
@@ -0,0 +1,17 @@
+teclado = {"A":2,"B":2,"C":2,"D":3,"E":3,"F":3,"G":4,"H":4,"I":4,"J":5,"K":5,"L":5,"M":6,"N":6,"O":6,"P":7,"Q":7,"R":7,"S":7,"T":8,"U":8,"V":8,"W":9,"X":9,"Y":9,"Z":9}
+
+while(True):
+    try:
+        tel = input().strip()
+        numero = ""
+        for i in tel:
+            if i in ["0","1","2","3","4","5","6","7","8","9"]:
+                numero += i
+            elif i == "*" or i == "#":
+                numero += i
+            elif i.upper() in teclado:
+                numero += str(teclado[i.upper()])
+        print(numero)
+            
+    except EOFError:
+        break
