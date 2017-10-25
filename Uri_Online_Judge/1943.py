cffb96c4107816a688b6d61139a9409e9400a401
Talitha Pumar <tapumar@gmail.com>
2017-10-25 14:41:28 -0200

    Create 1943.py

---
+++ Uri_Online_Judge/1943.py
@@ -0,0 +1,16 @@
+caso = int(input())
+
+if caso <=1:
+    print("Top 1")
+elif caso <=3:
+    print("Top 3")
+elif caso <=5:
+    print("Top 5")
+elif caso <=10:
+    print("Top 10")
+elif caso <=25:
+    print("Top 25")
+elif caso <=50:
+    print("Top 50")
+else:
+    print("Top 100")
