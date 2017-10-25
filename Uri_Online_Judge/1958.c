ffef25f1d794d0f918ac35d2692ccfddb03ab2d0
Talitha Pumar <tapumar@gmail.com>
2017-10-25 14:06:54 -0200

    Create 1958.c

---
+++ Uri_Online_Judge/1958.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    long double X;
+    char s[120];
+    scanf("%LE", &X);
+    sprintf(s,"%LE", X);
+    if(s[0] != '-') printf("+");
+    printf("%.4LE\n", X);
+    return 0;
+}
