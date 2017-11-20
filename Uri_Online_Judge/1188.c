92ddfbf8e5e6af1e0595c8cfad5982dff48232ac
Talitha Pumar <tapumar@gmail.com>
2017-11-20 12:29:02 -0200

    Create 1188.c

---
+++ Uri_Online_Judge/1188.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+	char ope;
+	int col,lin,i=11;
+	double matriz[12][12];
+	double soma=0,media;
+
+	scanf("%c", &ope);
+	
+	for(lin=0;lin<12;lin++){
+		for(col=0;col<12;col++){
+			scanf("%lf", &matriz[lin][col]);
+		}
+	}
+
+
+	for(lin=0;lin<12;lin++){
+		for(col=i+1;col<lin;col++){
+			soma += matriz[lin][col];
+		}
+		i--;
+	}
+
+	if(ope == 'M'){
+		media = soma/30;
+		printf("%.1lf\n", media);
+	}
+	else {
+		printf("%.1lf\n", soma);
+	}
+
+
+return 0;
+}
