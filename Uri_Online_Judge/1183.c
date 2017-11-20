978e5ba8facc281e0769a2caf9f76a9f5ebf66dd
Talitha Pumar <tapumar@gmail.com>
2017-11-20 12:29:52 -0200

    Create 1183.c

---
+++ Uri_Online_Judge/1183.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+	char ope;
+	int col,lin;
+	float matriz[12][12];
+	float soma=0,media;
+
+	scanf("%c", &ope);
+
+	for(lin=0;lin<12;lin++){
+		for(col=0;col<12;col++){
+			scanf("%f", &matriz[lin][col]);
+		}
+	}
+
+
+	for(lin=0;lin<12;lin++){
+		for(col=lin+1;col<12;col++){
+			soma += matriz[lin][col];
+		}
+	}
+	if(ope == 'M'){
+		media = soma/66;
+		printf("%.1f\n", media);
+	}
+	else {
+		printf("%.1f\n", soma);
+	}
+
+
+return 0;
+}
