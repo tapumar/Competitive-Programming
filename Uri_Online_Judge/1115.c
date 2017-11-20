34a500a57d5978a22d54197d0236bdc4db8d27b5
Talitha Pumar <tapumar@gmail.com>
2017-11-20 13:18:36 -0200

    Create 1115.c

---
+++ Uri_Online_Judge/1115.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int x,y;
+
+	scanf("%d %d", &x,&y);
+
+	while(x!=0 && y!=0){
+
+		if(x>0 && y>0){
+			printf("primeiro\n");
+		}
+		else if(x<0 && y>0){
+			printf("segundo\n");
+		}
+		else if(x>0 && y<0){
+			printf("quarto\n");
+		}
+		else if(x<0 && y<0){
+			printf("terceiro\n");
+		}
+		scanf("%d %d", &x,&y);
+	}
+ 
+    return 0;
+}
