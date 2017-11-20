8df6504f3ffd12b1a1a6adfa4bccc2bceca56f83
Talitha Pumar <tapumar@gmail.com>
2017-11-20 12:20:47 -0200

    Create 2483.c

---
+++ Uri_Online_Judge/2483.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int i,n;
+    scanf("%d", &n);
+    printf("Feliz nat");
+    for(i=0;i<n;i++){
+        printf("a");
+    }
+    printf("l!\n");
+ 
+    return 0;
+}
