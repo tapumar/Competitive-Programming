35c143223ca2b32425f73672678a6faec3516ee6
Talitha Pumar <tapumar@gmail.com>
2018-01-22 10:30:58 -0200

    Create 1003.cpp

---
+++ Uri_Online_Judge/1003.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+ int A,B,soma;
+
+ 	scanf("%d", &A);
+ 	scanf("%d", &B);
+
+ 	soma = A + B;
+
+ 	printf("SOMA = %d\n", soma);
+ 
+    return 0;
+}
