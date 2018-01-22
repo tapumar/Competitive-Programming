010bf170e1ae70c569b71c063edc95b0aead774b
Talitha Pumar <tapumar@gmail.com>
2018-01-22 10:32:14 -0200

    Create 1005.c

---
+++ Uri_Online_Judge/1005.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+float A,B,media;
+
+	scanf("%f", &A);
+	scanf("%f", &B);
+
+	media = ((A*3.5)+(B*7.5))/11;
+
+	printf("MEDIA = %.5f\n", media);
+
+
+    return 0;
+}
