bfb017726d6d327ecf9be94029af8108f5fb6070
Talitha Pumar <tapumar@gmail.com>
2018-01-22 10:32:40 -0200

    Create 1007.c

---
+++ Uri_Online_Judge/1007.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+int A,B,C,D,dif;
+
+	scanf("%d", &A);
+	scanf("%d", &B);
+	scanf("%d", &C);
+	scanf("%d", &D);
+
+	dif = (A*B)-(C*D);
+
+	printf("DIFERENCA = %d\n", dif);
+    return 0;
+}
