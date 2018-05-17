507df922760187c9f481666bae5646fcf922a7e0
Talitha Pumar <tapumar@gmail.com>
2018-05-17 08:57:07 -0300

    Create 2759.py

---
+++ Uri_Online_Judge/2759.py
@@ -0,0 +1,6 @@
+a = input()
+b = input()
+c = input()
+print("A = " + a + ", " + "B = " + b + ", " + "C = " + c)
+print("A = " + b + ", " + "B = " + c + ", " + "C = " + a)
+print("A = " + c + ", " + "B = " + a + ", " + "C = " + b)
