a791e7e24063d2f13778f1f9b055836ea6cee38b
Talitha Pumar <tapumar@gmail.com>
2018-09-10 18:29:03 -0100

    Delete maior_diferenca.py

--- CodeForces/lista_1_Programa-oCompetitivaUnB/maior_diferenca.py
+++
@@ -1,8 +0,0 @@
-n = input()
-vetor = input().split()
-max_diff = 0
-for i in range(len(vetor)-1):
-    diff = abs(int(vetor[i]) - int(vetor[i+1]))
-    if diff > max_diff:
-        max_diff = diff
-print(max_diff)
