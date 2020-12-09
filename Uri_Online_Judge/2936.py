c5a2b542b4275a9d1a3d07a0c8a2a8bc950cef7f
Talitha Pumar <tapumar@gmail.com>
2020-12-09 16:43:02 -0300

    Create 2936.py

---
+++ Uri_Online_Judge/2936.py
@@ -0,0 +1,10 @@
+curupira = int(input())*300
+boitata = int(input())*1500
+boto = int(input())*600
+mapinguari = int(input())*1000
+iara = int(input())*150
+chica = 225
+
+total = curupira + boitata + boto + mapinguari + iara + chica
+
+print(total)
