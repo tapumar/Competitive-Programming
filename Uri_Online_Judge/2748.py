307f0e260e215e91dce78bda31aefd5ba08545fb
Talitha Pumar <tapumar@gmail.com>
2020-12-09 16:24:11 -0300

    Create 2748.py

---
+++ Uri_Online_Judge/2748.py
@@ -0,0 +1,23 @@
+for traco in range(39):
+    print("-", end="")
+print()
+print("|", end="")
+print("        Roberto                      ", end="")
+print("|")
+print("|", end="")
+for espaco in range(37):
+    print(" ", end="")
+print("|")
+print("|", end="")
+print("        5786                         ", end="")
+print("|")
+print("|", end="")
+for espaco in range(37):
+    print(" ", end="")
+print("|")
+print("|", end="")
+print("        UNIFEI                       ", end="")
+print("|")
+for traco in range(39):
+    print("-", end="")
+print()
