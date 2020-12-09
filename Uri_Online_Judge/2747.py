dbe8caabf0bd101150864a19d81c0ace2edfce8a
Talitha Pumar <tapumar@gmail.com>
2020-12-09 16:18:53 -0300

    Create 2747.py

---
+++ Uri_Online_Judge/2747.py
@@ -0,0 +1,10 @@
+for traco in range(39):
+    print("-", end="")
+print()
+for linha in range(5):
+    print("|", end="")
+    for espaco in range(37):
+        print(" ", end="")
+    print("|")
+for traco in range(39):
+    print("-", end="")
